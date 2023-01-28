52266ba61c630390e9a9f65845cc32225e74d30b
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2023-01-28 02:06:52 -0500

    finished basics of last task

--- index.js
+++ index.js
@@ -178,6 +178,14 @@ const sortedGames = GAMES_JSON.sort((item1, item2) => {
 
 // use destructuring and the spread operator to grab the first and second games
 
+let [ first_game, second_game, ...rest ] = GAMES_JSON;
+
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const first_game_element = document.createElement("p");
+first_game_element.textContent = `${first_game.name}`
+firstGameContainer.appendChild(first_game_element);
 
 // do the same for the runner up item
+const second_game_element = document.createElement("p");
+second_game_element.textContent = `${second_game.name}`
+secondGameContainer.appendChild(second_game_element);
